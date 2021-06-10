93a1e7f1c925bc3558a0a813a54827bacad7e5fa
carlosatsimoes <79661015+carlosatsimoes@users.noreply.github.com>
2021-06-10 19:59:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ data_atual = datetime.now()
 
 M = "Mário"
 F = "Fábio"
+C = "Carlos"
 F_time = "2021-06-10 01:08:57"
 
 print(f"OK. Confirmado ! - {M} : {data_atual}")
 print(f"OK. Confirmado ! - {F} : {F_time}")
-
+print(f"Ok. Confirmado ! - {C} : {data_atual}")
 
