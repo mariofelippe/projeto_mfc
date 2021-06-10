f2c39a1ae017cc45aebb5f7ffd0556f03f580a61
fabiojspereira <fabiojspereira@gmail.com>
2021-06-10 01:53:08 -0300

    Novas modificações ...

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ data_atual = datetime.now()
 
 M = "Mário"
 F = "Fábio"
+F_time = "2021-06-10 01:08:57"
 
 print(f"OK. Confirmado ! - {M} : {data_atual}")
-print(f"OK. Confirmado ! - {F}")
+print(f"OK. Confirmado ! - {F} : {F_time}")
+
+
